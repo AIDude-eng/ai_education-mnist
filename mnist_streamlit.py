96cd6abda40195bd66c05ca8cbf5ab412b51dd04
Florian Engertsberger <florian.engertsberger@gmail.com>
2021-01-22 18:03:11 +0100

    grammar

--- mnist_streamlit.py
+++ mnist_streamlit.py
@@ -12,17 +12,27 @@ import torch.optim as optim
 from torchvision import datasets, transforms
 from types import SimpleNamespace
 
+
+logo = Image.open(os.path.join('img', 'logo.png'))
+
+st.sidebar.image(logo)
+st.sidebar.write("Gehe rechts die Aufgaben Schritt für Schritt durch. Wenn du auf die kleinen Kästchen drückst, "
+                 "erscheint der nächste Bereich.")
+
+st.image(logo)
+
 st.title("KI entwickeln")
-st.write("### Wir werden nun gmeinsam Schritt für Schritt eine KI entwickeln. Die Aufgabe der KI wird es sein,"
-         "handgeschriebene Zahlen zu klassifizieren. Das heißt wenn wir dem Computer ein Bildes einer Zahl zeigen,"
-         "soll er sagen, welche Zahl abgebildet ist. Das klingt vielleicht für manche einfach, für einen Computer ist"
+st.write("### Wir werden nun gemeinsam Schritt für Schritt eine KI entwickeln. Genauer gesagt werden wir Maschinelles Lernen betreiben. "
+         "Die Aufgabe der KI wird es sein, "
+         "handgeschriebene Zahlen zu klassifizieren. Das heißt wenn wir dem Computer ein Bild einer Zahl zeigen,"
+         "soll er sagen, welche Zahl abgebildet ist. Das klingt vielleicht für manche einfach, für einen Computer ist "
          "das jedoch gar nicht so leicht.")
 
-st.write("### Wie wir gelernt haben, brauchen wir um eine KI zu trainieren viele Daten. In unserem Fall brauchen wir "
-         "viele Bilder von handgeschriebenen Zahlen. Glücklicherweise gibt es davon ein riesiges Datenset namesn MNIST. "
-         "Im ersten Schritt werden wir also unser Datenset laden und schauen wie viele Elemente es beinhaltet.")
+st.write("### Wie wir gelernt haben, brauchen wir um eine KI zu trainieren, viele Daten. In unserem Fall brauchen wir "
+         "viele Bilder von handgeschriebenen Zahlen. Glücklicherweise gibt es davon sehr viele Daten. "
+         "Im ersten Schritt werden wir die Daten, also die Bilder, laden und schauen wie viele es sind.")
 
-st.write("## MNIST Datenset Laden")
+st.write("## Daten Laden")
 
 x_train = np.load(os.path.join("data_small", "x_test.npy"))
 y_train = np.load(os.path.join("data_small", "y_test.npy"))
@@ -33,27 +43,28 @@ args = SimpleNamespace(batch_size=64, test_batch_size=1000, epochs=1,
 torch.manual_seed(args.seed)
 device = 'cpu'
 
-load_data = st.checkbox(label="Datenset laden")
+load_data = st.checkbox(label="Daten laden")
 
 if load_data:
-    st.write("### Du hast das MNIST Datenset erfolgreich geladen!")
-    st.markdown("### **Zusammenfassung des Datenset:**")
-    st.write(f"### Das Datenset besteht aus 70,000 Bildern.")
+    st.write("### Du hast die Bild Daten erfolgreich geladen!")
+    st.markdown("### **Zusammenfassung:**")
+    st.write(f"### Die Daten bestehen aus 70,000 Bildern.")
     st.write(f"### Jedes Bild ist 28 x 28 Pixel groß.")
 st.write(" ")
 
 if load_data:
-    b1 = st.checkbox("Nächster Schritt: Datenset abbilden")
+    b1 = st.checkbox("Nächster Schritt: Daten abbilden")
 
 if load_data and b1:
     st.write("## Datenset abbilden")
     st.write("### Als nächstes möchtest du dir sicher ein paar Beispiel Bilder von deinem Datenset ansehen!")
 
-    n_img = st.selectbox("Wähle aus wie viele Bilder du ansehen möchtest:", options=[0, 10, 15, 20])
+    n_img = st.selectbox("Wähle aus wie viele Bilder du ansehen möchtest:", options=[0, 10, 15, 20, 30])
     rows = int(n_img/5)
 
 if load_data and b1 and n_img != 0:
     fig, ax = plt.subplots(rows, 5)
+    fig.set_figheight(1+1.2*rows)
     fig.suptitle("Beispiel Bilder")
     i = 0
     for x in range(rows):
@@ -82,6 +93,9 @@ if load_data and b1 and b2:
     two_d = (np.reshape(x_train[50], (28, 28)) * 255).astype(np.uint64)
     two_d_l = y_train[50]
     fig = plt.figure()
+    plt.xticks([])
+    plt.yticks([])
+    fig.set_figheight(2)
 
     st.write("### Welche Nummer ist unten abgebildet?")
     answer = st.selectbox(label="Wähle eine Nummer aus:", options=[i for i in range(10)])
@@ -96,8 +110,9 @@ if load_data and b1 and b2:
 
 if load_data and b1 and b2:
     st.write("### Für uns Menschen ist diese Aufgabe sehr einfach. Für einen Computer jedoch nicht ganz so leicht. Dieser hat "
-             "kein Verständnich wie wir Mesnchen und muss sich Pixel für Pixel des Bildes ansehen um zu wissen, welche Zahl"
-             " abgebildet ist. Wir werden in den nächsten Schritten nun gemeinsam eine KI programmieren welche diese "
+             "kein Verständnis wie wir Mesnchen und muss sich Pixel für Pixel des Bildes ansehen um zu wissen, welche Zahl"
+             " abgebildet ist. Wir werden in den nächsten Schritten nun gemeinsam eine KI"
+             " trainieren welche diese "
              "handgeschriebenen Zahlen benennen kann. Los gehts!")
 
 st.write(" ")
@@ -126,10 +141,10 @@ if load_data and b1 and b2 and b3:
         nn = Image.open(os.path.join('img', 'nn.PNG'))
         st.image(nn, caption="Neuronales Netzwerk", use_column_width=True)
         st.write("### Das ist ein Bild eines Neuronalen Netzwerks. Diese heißen so, weil sie so ähnlich funktionieren, "
-                 "wie die Neuronen in unserem Gehirn. Neuronen sind unsere Gehirnzelen und diese leiten Informationen "
+                 "wie die Neuronen in unserem Gehirn. Neuronen sind unsere Gehirnzellen und diese leiten Informationen "
                  "in unserem Körper weiter. Das gleiche passiert auch hier. Ganz links werden die Pixel der Bilder "
                  "aufgenommen. In der Mitte wird das Bild verarbeitet und rechts spuckt das Neuronale Netz dann "
-                 "aus welche Zahl es ist oder zumindest glaubt dass es ist.")
+                 "aus, welche Zahl es ist oder zumindest glaubt dass es ist.")
     if clf_name == "CNN":
         accuracy = 95.60
         model = model = CNN(use_batch_norm=True, n_blocks=3, n_layers=1, channels=16, multiply_channels=1,
@@ -139,7 +154,7 @@ if load_data and b1 and b2 and b3:
         st.write("### CNN ist eine Abkürzung und steht für \"Konvolutionales Neuronales Netz\". Hier steckt im "
                  "Namen auch ein Neuronales Netz drinnen. Diese Art ist aber schon etwas komplizierter, "
                  "also werden wir dieses nicht genauer erklären. Merk dir einfach, dass CNNs vor allem "
-                 "für die Klassifizierung von Bildern, also genau das was wir vor haben, sehr gut sind.")
+                 "für die Klassifizierung von Bildern sehr gut geeignet sind.")
 
 st.write(" ")
 if load_data and b1 and b2 and b3:
@@ -152,7 +167,7 @@ if load_data and b1 and b2 and b3 and b4:
              "viele Beispiel Bilder aus unserem Datenset und jeweils die dazugehörige Klasse, also bei uns die Zahl die auf dem Bild "
              "zu sehen ist. Die KI lernt mit jedem Bild dazu. Manchmal liegt sie richtig und manchmal falsch. Je"
              "nachdem ändert sie manche Parameter und versucht es erneut. Das geht so lange bis die KI "
-             "nicht mehr dazu lernt.")
+             "nicht mehr dazulernt.")
 
     st.write("### Drücke den Knopf unten um deine KI zu trainieren.")
     training = st.button(label="KI trainieren")
@@ -161,7 +176,7 @@ if load_data and b1 and b2 and b3 and b4:
     st.write("### Hier passiert nun sehr viel im Hintergrund. Merk dir einfach, dass deine KI nun mit den Bildern, die wir "
              "oben gesehen haben, trainiert wurde und diese nun klassifizieren kann. Das heißt wenn wir der KI ein Bild "
              "mit einer Zahl zeigen, kann sie uns sagen, welche Zahl zu sehen ist. Das funktionerier leider meistens nicht "
-             "perfekt. Das heißt die KI macht manchmal Fehler und liegt nicht richtig. Sehen wir uns nun an wie gut unsere Ki ist.")
+             "perfekt. Das heißt die KI macht manchmal Fehler und liegt nicht richtig. Sehen wir uns nun an wie gut unsere KI ist.")
 
 st.write(" ")
 if load_data and b1 and b2 and b3 and b4:
@@ -176,7 +191,9 @@ if load_data and b1 and b2 and b3 and b4 and b5:
              "Wert ist desto besser.")
 
     acc = st.checkbox(label="Genaugkeit")
-    if acc:
+    if clf_name == "Nichts":
+        st.write("### Du musst oben zuerst ein Modell auswählen!")
+    elif acc:
         st.write(f"### Deine KI hat eine Genauigkeit von **{accuracy}%**!")
         st.write(f"### Das heißt deine KI würde im Durchschnitt von 100 Bildern ungefähr **{int(accuracy)}** richtig klassifizieren!")
 
@@ -193,9 +210,8 @@ if load_data and b1 and b2 and b3 and b4 and b5 and b6:
         two_d = (np.reshape(x_train[slider], (28, 28)) * 255).astype(np.float32)
         two_d_l = y_train[slider]
         fig = plt.figure()
-        plt.title(f"Dieses Bild zeigt die Nummer {two_d_l}")
+        plt.title(f"{two_d_l}")
         plt.imshow(two_d, cmap='gray_r')
-        st.pyplot(fig)
 
         two_d = torch.tensor(two_d)
         model.eval()
@@ -212,9 +228,8 @@ if load_data and b1 and b2 and b3 and b4 and b5 and b6:
         two_d = (np.reshape(x_train[slider], (28, 28)) * 255).astype(np.float32)
         two_d_l = y_train[slider]
         fig = plt.figure()
-        plt.title(f"Dieses Bild zeigt die Nummer {two_d_l}")
+        plt.title(f"{two_d_l}")
         plt.imshow(two_d, cmap='gray_r')
-        st.pyplot(fig)
 
         two_d = np.reshape(x_train[slider], (28, 28)).astype(np.float32)
         two_d = torch.tensor(two_d)
@@ -229,8 +244,13 @@ if load_data and b1 and b2 and b3 and b4 and b5 and b6:
             pred = output.max(1, keepdim=True)[1]
             pred = int(pred)
 
+    if clf_name == "Nichts":
+        st.write("### Du musst oben zuerst ein Modell auswählen!")
 
-    st.write(f"## KI Klassifizierung: **{pred}**")
+    if not clf_name == "Nichts":
+        st.write(f"## KI Klassifizierung: **{pred}**")
+        st.write(f"## Richtige Zahl: **{two_d_l}**")
+        st.pyplot(fig)
 
 # TODO
 #- Merken_ Bild mit index 8, sieht interessant aus
